bcd98dcc7ae41ddf11c3f610e9972c4477fcc7bf
bdmg114 <bdmg114@gmail.com>
2025-05-10 22:21:44 +0900

    :fire: rm design table

--- src/database/models.py
+++ src/database/models.py
@@ -1,12 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from .connect import DB_props, MODEL_BASE
-
-class Design(DB_props.get_instance().ModelBase):
-    __tablename__ = "design"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True)
-    description = Column(String)
+from .connect import DB_props
 
 class ReceiverGroup(DB_props.get_instance().ModelBase):
     __tablename__ = "receiver_group"
@@ -21,10 +15,9 @@ class Letter(DB_props.get_instance().ModelBase):
     abbr_sender = Column(String, index=True)
     abbr_receiver = Column(String, index=True)
     group_receiver_id = Column(Integer, ForeignKey("receiver_group.id"))
-    design_id = Column(Integer, ForeignKey("design.id"))
+    design_id = Column(Integer)
     content = Column(String)
     content_secret = Column(String)
     pin = Column(String)
 
     group_receiver = relationship("ReceiverGroup")
-    design = relationship("Design")
