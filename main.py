012f6ba87f3fa713530ffcd0eb96bf8c1190344b
bdmg114 <bdmg114@gmail.com>
2025-05-10 17:19:37 +0900

    Refactor main.py to include letter and search routers, and update root endpoint message

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from fastapi import FastAPI
+from src.routers import letter, search
 
 app = FastAPI()
 
+app.include_router(letter.router, prefix="/api/letter", tags=["Letter"])
+app.include_router(search.router, prefix="/api/search", tags=["Search"])
+
 @app.get("/")
 async def root():
-    return {
-        "message": "SUNSHINE TUNA KIMBAP SKYST"
-    }
-
-@app.get("/favicon.ico")
-async def favicon():
-    return {}
-
-
+    return {"message": "ㄱㄷ에게!"}
