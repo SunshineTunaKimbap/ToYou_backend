ccfffe9e0b50132898673f92c3fad2c4d03c2549
bdmg114 <bdmg114@gmail.com>
2025-05-10 20:04:27 +0900

    :fire: 안쓰는 파일 삭제

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from src.routers import letter, search, post
 
 app = FastAPI()
 
-app.include_router(letter.router, prefix="/api/letter", tags=["Letter"])
 app.include_router(search.router, prefix="/api/search", tags=["Search"])
 app.include_router(post.router, prefix="/api/post", tags=["Post"])
 
