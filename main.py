0ec643de7a5e60cfdbde4cf99323fd5113135b24
bdmg114 <bdmg114@gmail.com>
2025-05-10 21:02:04 +0900

    :construction: fixing..

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from src.routers import search, post
-#from src.database.connect import DB_props, get_db
-#from src.database.models import Letter
+from src.database.connect import DB_props, get_db
+from src.database.models import Letter
 
-#DB = DB_props.get_instance()
+DB = DB_props.get_instance()
 app = FastAPI()
 
 app.include_router(search.router, prefix="/api/search", tags=["Search"])
 app.include_router(post.router, prefix="/api/post", tags=["Post"])
 
-#@app.get("/")
-#async def root(db: Session = Depends(get_db)):
-#    return db.query(Letter).all()
+@app.get("/")
+async def root(db: Session = Depends(get_db)):
+    return db.query(Letter).all()
