f5f626a987e3ff924888968b1521aeb4a2b09c54
bdmg114 <bdmg114@gmail.com>
2025-05-10 17:51:01 +0900

    :sparkles: wrote mockup code for posting letter

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from src.routers import letter, search
+from src.routers import letter, search, post
 
 app = FastAPI()
 
 app.include_router(letter.router, prefix="/api/letter", tags=["Letter"])
 app.include_router(search.router, prefix="/api/search", tags=["Search"])
+app.include_router(post.router, prefix="/api/post", tags=["Post"])
 
 @app.get("/")
 async def root():
