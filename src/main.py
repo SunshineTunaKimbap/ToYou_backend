a08f84825ebbab7dbfcca3847dffb5d9d245fac3
bdmg114 <bdmg114@gmail.com>
2025-05-10 16:56:35 +0900

    중복 파일 삭제제

--- src/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from routers import letter, search
-
-app = FastAPI()
-
-app.include_router(letter.router, prefix="/api/letter", tags=["Letter"])
-app.include_router(search.router, prefix="/api/search", tags=["Search"])
